bc8f52c8522da87a66e4fa313d304e02f82752b8
Sabina Schmitz <sabinahschmitz@gmail.com>
2022-07-21 09:36:44 +0200

    setup package over terminal

---
+++ vuejsfirebase/src/ButtonCounter.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button class="btn" @click="count++">You clicked me {{ count }} times.</button>
+</template>
+
+<style scoped>
+.btn {
+  color: #fff
+}
+</style>
